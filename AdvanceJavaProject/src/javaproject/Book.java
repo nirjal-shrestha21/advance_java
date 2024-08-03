5ece5661e2e757f96879875bfc5b9cd7940a46bd
nirjal-shrestha21 <nirjal.shrestha021@apexcollege.edu.np>
2024-08-03 21:34:41 +0545

    Add files via upload

---
+++ AdvanceJavaProject/src/javaproject/Book.java
@@ -0,0 +1,17 @@
+package AdvanceJavaProject.src.javaproject;
+
+public class Book {
+    private String author;
+    private String title;
+    private String publication;
+    private String subject;
+
+    public Book(String author, String title, String publication, String subject) {
+        this.author = author;
+        this.title = title;
+        this.publication = publication;
+        this.subject = subject;
+    }
+
+    // Getters and setters
+}
